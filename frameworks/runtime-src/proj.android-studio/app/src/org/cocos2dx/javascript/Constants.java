d0423eb3490d4df239d5bbc94f00e33b1a16404d
Apocalypse <1025548262@qq.com>
2021-05-07 19:04:48 +0800

    update：oppo版本号

--- frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/javascript/Constants.java
+++ frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/javascript/Constants.java
@@ -20,13 +20,13 @@ public enum Constants {
     public static final String appId = "wxc1237e9442f71a99";
     public static final String sdk_serial = "600bbeaeb3b4f6635de190f8";
 
-    public static final String s1 = "hw";
+    public static final String s1 = "oppo";
 
-    public static final  String videoID = "945990831"; //shxs 945927425 myx 945898936 szrc 945955778 gzyh 945979859 hw 945990831 oppo 946045794 ks 946011282
+    public static final  String videoID = "946045794"; //shxs 945927425 myx 945898936 szrc 945955778 gzyh 945979859 hw 945990831 oppo 946045794 ks 946011282
     public static final  String bannerID = "";//shxs 945927419 myx 945898927 szrc 945955765 gzyh
-    public static final  String nativeID = "945990828";//shxs 945927411 myx 945898923 szrc 945955754 gzyh 945979854 hw 945990828 oppo 946045797 ks 946011424
-    public static final  String interactionID = "945990830";//shxs 945927423 myx 945898930 szrc 945955770 gzyh 945979857 hw 945990830 oppo 946045796 ks 946011264
-    public static final  String splashID = "887458000";//shxs 887449241 myx 887445670 szrc 887453267 gzyh 887456207 hw 887458000 oppo 887465616 ks 887460599
+    public static final  String nativeID = "946045797";//shxs 945927411 myx 945898923 szrc 945955754 gzyh 945979854 hw 945990828 oppo 946045797 ks 946011424
+    public static final  String interactionID = "946045796";//shxs 945927423 myx 945898930 szrc 945955770 gzyh 945979857 hw 945990830 oppo 946045796 ks 946011264
+    public static final  String splashID = "887465616";//shxs 887449241 myx 887445670 szrc 887453267 gzyh 887456207 hw 887458000 oppo 887465616 ks 887460599
 
     public static boolean isDeepLink = false;
 }
